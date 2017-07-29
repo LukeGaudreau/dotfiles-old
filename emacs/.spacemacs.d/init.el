acb1c5a5914cc76216c4ff6d4c5d25a8d8d83d21
Luke Gaudreau <luke@infosoph.org>
2017-07-29 10:54:21 -0400

    Add twitter

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -42,7 +42,6 @@ values."
      ivy
      auto-completion
      better-defaults
-     common-lisp
      (elfeed :variables
              rmh-elfeed-org-files (list "~/Notes/feeds.org")
              elfeed-goodies/entry-pane-position 'bottom)
@@ -57,6 +56,7 @@ values."
             shell-default-height 30
             shell-default-position 'bottom)
      syntax-checking
+     twitter
      version-control
      )
    ;; List of additional packages that will be installed without being
@@ -321,8 +321,6 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  ;; Slime
-  (load (expand-file-name "~/quicklisp/slime-helper.el"))
   ;; Notmuch
   (setq user-full-name "Luke Gaudreau"
         user-mail-address "luke.gaudreau@bc.edu"
@@ -337,6 +335,8 @@ you should place your code here."
                                  (:name "drafts" :query "tag:draft" :key "d" :sort-order 'newest-first)
                                  (:name "archive" :query "tag:archive" :key "a" :sort-order 'newest-first)))
 
+  ;; twitter
+  (setq twittering-use-master-password t)
   ;; org-mode
   (require 'ox-confluence)
   (setq org-startup-indented t
@@ -412,7 +412,6 @@ you should place your code here."
                               (org-tags-match-list-sublevels nil)
                               ))
                        )))))))
-  
   ;; Do not write anything past this comment. This is where Emacs will
   ;; auto-generate custom variable definitions.
   (custom-set-variables
@@ -451,33 +450,3 @@ you should place your code here."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#d2ceda" "#f2241f" "#67b11d" "#b1951d" "#3a81c3" "#a31db1" "#21b8c7" "#655370"])
- '(beacon-color "#F8BBD0")
- '(evil-emacs-state-cursor (quote ("#D50000" hbar)) t)
- '(evil-insert-state-cursor (quote ("#D50000" bar)) t)
- '(evil-normal-state-cursor (quote ("#F57F17" box)) t)
- '(evil-visual-state-cursor (quote ("#66BB6A" box)) t)
- '(evil-want-Y-yank-to-eol nil)
- '(highlight-indent-guides-auto-enabled nil)
- '(highlight-symbol-colors
-   (quote
-    ("#F57F17" "#66BB6A" "#0097A7" "#42A5F5" "#7E57C2" "#D84315")))
- '(highlight-symbol-foreground-color "#546E7A")
- '(highlight-tail-colors (quote (("#F8BBD0" . 0) ("#FAFAFA" . 100))))
- '(notmuch-hello-sections
-   (quote
-    (notmuch-hello-insert-search notmuch-hello-insert-saved-searches notmuch-hello-insert-recent-searches notmuch-hello-insert-alltags notmuch-hello-insert-footer)))
- '(org-agenda-files (quote ("~/Notes/Support.org" "~/Notes/Projects.org")))
- '(package-selected-packages
-   (quote
-    (doom-themes twittering-mode web-beautify livid-mode skewer-mode json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc company-tern dash-functional tern coffee-mode web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode haml-mode emmet-mode company-web web-completion-data slime-company common-lisp-snippets writeroom-mode mu4e-maildirs-extension mu4e-alert material-theme notmuch-theme notmuch halcyon-light-theme slime spotify color-theme-sanityinc-tomorrow apropospriate-theme zerodark-theme all-the-icons memoize font-lock+ php-extras phpunit phpcbf php-auto-yasnippets drupal-mode php-mode magit-gh-pulls github-search github-clone github-browse-file gist gh marshal logito pcache ht xterm-color unfill smeargle shell-pop orgit org-projectile org-present org-pomodoro alert log4e gntp org-download mwim multi-term mmm-mode markdown-toc markdown-mode magit-gitflow htmlize gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit with-editor eshell-z eshell-prompt-extras esh-help elfeed-web simple-httpd elfeed-org elfeed-goodies ace-jump-mode noflet elfeed diff-hl company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-make helm helm-core google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump popup f s diminish define-word counsel-projectile projectile pkg-info epl counsel swiper ivy column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash async aggressive-indent adaptive-wrap ace-window ace-link avy)))
- '(pos-tip-background-color "#ffffff")
- '(pos-tip-foreground-color "#78909C")
- '(send-mail-function (quote sendmail-send-it))
- '(tabbar-background-color "#ffffff"))
