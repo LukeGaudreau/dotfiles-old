3e85ac4b8866e3d92d32b8dac5a7dace6ccf00a3
Luke Gaudreau <luke@infosoph.org>
2017-08-18 10:01:31 -0400

    Add zerodark and switch to frames-only-mode

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -62,7 +62,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(frames-only-mode all-the-icons zerodark-theme)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -134,14 +134,15 @@ nomtuchvalues."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(halcyon-light
-                         spacemacs-light
-                         spacemacs-dark)
+   dotspacemacs-themes '(zerodark
+                         halcyon-light
+                         spacemacs-dark
+                         spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Ubuntu Mono"
+   dotspacemacs-default-font '("iosevka"
                                :size 28
                                :weight normal
                                :width normal
@@ -320,6 +321,10 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; Let dwm manage windows
+  (frames-only-mode)
+  ;; Use zerodark modeline
+  ;;(zerodark-setup-modeline-format)
   ;; Notmuch
   (setq user-full-name "Luke Gaudreau"
         user-mail-address "luke.gaudreau@bc.edu"
